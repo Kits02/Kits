3a03e378cb0a8abb7a120ad7ba0e02a17b858312
Kits02 <paquitojrbinongo6@gmail.com>
2024-11-26 04:14:57 -0500

    Update Kits.py

--- Kits.py
+++ Kits.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 # Function to display biography with image
-def display_bio(name, title, location, education, skills, philosophy, mission, vision, image_path):
+def display_bio(name, title, location, education, skills, philosophy, mission, vision, image_url):
     st.title(f"**{name}**")
     st.header(f"**{title}**")
     
-    # Display the image
-    st.image(image_path, caption=f"Photo of {https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/461971567_526586080318564_4016883153565424525_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGmcIZOJD3AHDAiYPYA4qVAf1ZpL3kXZsl_VmkveRdmyVZ4Nc84RsDJaW8BmunSKQXP07-N9Q7FpV_oves5ONms&_nc_ohc=GUKKYBmRSU0Q7kNvgEEjBc7&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=AoSXPKF8uLxZF_CKRnqAkW9&oh=00_AYCPOuIEBdfxMDM6MIpJJZr-PfSzEd0DCUCvm6Q4V_DqNg&oe=674B6CC1}", use_column_width=True)
+    # Display the image using the URL
+    st.image(image_url, caption=f"Photo of {name}", use_column_width=True)
     
     st.subheader("Location:")
     st.write(location)
@@ -51,7 +51,7 @@ bio_data = {
         "Becoming a sought-after creative professional while balancing academic growth and "
         "practical experience."
     ),
-    "image_path": "path_to_your_image.jpg",  # Update this with the path to your image
+    "image_url": "https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/461971567_526586080318564_4016883153565424525_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeGmcIZOJD3AHDAiYPYA4qVAf1ZpL3kXZsl_VmkveRdmyVZ4Nc84RsDJaW8BmunSKQXP07-N9Q7FpV_oves5ONms&_nc_ohc=GUKKYBmRSU0Q7kNvgEEjBc7&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=AoSXPKF8uLxZF_CKRnqAkW9&oh=00_AYCPOuIEBdfxMDM6MIpJJZr-PfSzEd0DCUCvm6Q4V_DqNg&oe=674B6CC1",
 }
 
 # Streamlit app
@@ -65,7 +65,7 @@ def main():
         bio_data["philosophy"],
         bio_data["mission"],
         bio_data["vision"],
-        bio_data["image_path"]  # Provide the image path
+        bio_data["image_url"]  # Provide the image URL
     )
 
 if __name__ == "__main__":
